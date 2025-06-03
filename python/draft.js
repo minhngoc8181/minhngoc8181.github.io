1a55a71423d31333c8164547d8846758adf07846
ngoc.ha <ngoc.ha@eiu.edu.vn>
2025-06-04 00:00:35 +0700

    Refactor code structure for improved readability and maintainability

---
+++ python/draft.js
@@ -0,0 +1,6 @@
+//https://www.online-python.com/
+
+$('#sidebar-left-ad, #sidebar-right-ad, nav, #header-control,#info,#footer').remove();
+$('#container').css({ 'width': '100vw', 'display': 'flex' });
+$('#mi').css({ height: '99%', width: '55%' });
+$('#d').css({ height: '99%', width: '45%' });
